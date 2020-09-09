19c250c62d3d402e7a4793a3b56077cd0a08bd60
Raymundo Cruz T <rcruz@novasolutionsystems.com>
2020-09-09 11:22:36 -0500

    allow origin *

--- src/main/java/com/walkoding/websocketsnosecurity/WebSocketConfiguration.java
+++ src/main/java/com/walkoding/websocketsnosecurity/WebSocketConfiguration.java
@@ -18,7 +18,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
     }
 
 //    @Override
